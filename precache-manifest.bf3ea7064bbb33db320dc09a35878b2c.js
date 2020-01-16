ee7686e4a3aa5924937f1ce0ba903534146d9ac8
Michael Robil <ch.michaelsamir@hotmail.com>
2020-01-16 17:24:29 -0600

    Updates

---
+++ precache-manifest.bf3ea7064bbb33db320dc09a35878b2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672acc75b3ef6ce0db888a1f818b650b",
+    "url": "//dsomething.cloudfront.net/index.html"
+  },
+  {
+    "revision": "06cb1628be0a7ac2b02a",
+    "url": "//dsomething.cloudfront.net/static/css/main.a582e99b.chunk.css"
+  },
+  {
+    "revision": "3667f77e4d50f17bc388",
+    "url": "//dsomething.cloudfront.net/static/js/2.9d78d09f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "//dsomething.cloudfront.net/static/js/2.9d78d09f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06cb1628be0a7ac2b02a",
+    "url": "//dsomething.cloudfront.net/static/js/main.5698aaea.chunk.js"
+  },
+  {
+    "revision": "93b10dfd1a1e23d67734",
+    "url": "//dsomething.cloudfront.net/static/js/runtime-main.bc678d41.js"
+  }
+]);
