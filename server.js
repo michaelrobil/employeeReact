1c9ef2278c0575b521b714be71b383b096c7b675
Michael Robil <ch.michaelsamir@hotmail.com>
2020-01-16 17:47:41 -0600

    changed file

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
