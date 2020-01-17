aadea29cba00072c43d29e9a1a917d6385106bc6
Michael Robil <ch.michaelsamir@hotmail.com>
2020-01-16 19:19:03 -0600

    react-create-app on Heroku

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const { Server } = require('ws');
+
+const wss = new Server({ server });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,7 +15,19 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+var HOST = location.origin.replace(/^http/, 'ws')
+var ws = new WebSocket(HOST);
+var el;
+
+ws.onmessage = function (event) {
+  el = document.getElementById('server-time');
+  el.innerHTML = 'Server time: ' + event.data;
+};
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    ws.on('close', () => console.log('Client disconnected'));
+  });
