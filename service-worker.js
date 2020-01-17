f70aae12bfbf2daa2343b34e128aac0818409eb4
Michael Robil <ch.michaelsamir@hotmail.com>
2020-01-16 18:07:17 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employeereact/precache-manifest.ae46275fb790510a36e36f4f01345d6b.js"
+  "https://michaelrobil.github.io/employeereact/precache-manifest.d6bd7ddacc0ca50d552ce294f08ae615.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employeereact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://michaelrobil.github.io/employeereact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
